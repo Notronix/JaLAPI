53c2d680fc80e717985dde620f12325b66db8a1f
clint.munden <clint.munden@notronix.com>
2019-01-24 17:10:13 -0330

    - version 0.0.39 - fixed Instant deserialization

--- src/main/java/com/notronix/lw/gson/InstantDeserializer.java
+++ src/main/java/com/notronix/lw/gson/InstantDeserializer.java
@@ -7,25 +7,21 @@ import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InstantDeserializer implements JsonDeserializer<Instant>
 {
-    private static Pattern PATTERN = Pattern.compile(".*\\.[\\d]{2}([\\d]*)$");
+    private static Pattern MISSING_Z_FORMAT = Pattern.compile("[\\d]{4}-[\\d]{2}-[\\d]{2}T[\\d]{2}:[\\d]{2}:[\\d]{2}[.\\d]*$");
 
     @Override
     public Instant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         try {
-            String date = json.getAsString().replaceFirst("[\\d]{5}\\+00:00$", "Z");
-            Matcher matcher = PATTERN.matcher(date);
-
+            String date = json.getAsString().replaceFirst("\\+00:00$", "Z");
+            Matcher matcher = MISSING_Z_FORMAT.matcher(date);
             if (matcher.matches()) {
-                String group = matcher.group(1);
-                date = date.replaceFirst(group + "$", "Z");
+                date += "Z";
             }
 
             return Instant.parse(date);
